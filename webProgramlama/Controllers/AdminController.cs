33d15cd50254a66c5ad4ab607ede004efb9324be
yusrasengun4 <132217043+yusrasengun4@users.noreply.github.com>
2024-12-02 17:14:36 +0300

    Proje dosyası ekle.

---
+++ webProgramlama/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webProgramlama.Controllers
+{
+	public class AdminController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ webProgramlama/Models/Calisan.cs
@@ -0,0 +1,7 @@
+﻿namespace webProgramlama.Models
+{
+	public class Calisan
+	{
+		public string CalisanAD { get; set; }
+	}
+}
