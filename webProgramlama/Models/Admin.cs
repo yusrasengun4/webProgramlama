4240c5c16dd4c09948dca8c3beb4ea0771c78840
yusrasengun4 <132217043+yusrasengun4@users.noreply.github.com>
2024-12-07 01:18:55 +0300

    Admin işlemleri

---
+++ webProgramlama/Models/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webProgramlama.Models
+{
+	public class Admin
+	{
+		[Key]
+		public int AdminId
+		{
+			get; set;
+		}
+
+		public string AdminMail { get; set; }
+
+
+		public string Sifre { get; set; }
+		public string AdminRol { get; set; }
+	}
+}
+
